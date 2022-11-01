2a3c7a32c0894cc4bbad68632db276538c097b38
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-11-01 21:43:23 +0300

    add8t

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form')
 
+formRef.addEventListener('submit', onTrueSubmit);
+ 
+function onTrueSubmit(event) {
+    event.preventDefault();
+    
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        return alert('Заповни поля');
+    }
+    const result = {
+        email: email.value,
+        password: password.value
+};
+    console.log(result);
+    event.currentTarget.reset();
+
+    
+}
