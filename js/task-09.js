520ecdd19e43a30b6125436fae5a0896201f80bf
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-11-01 22:47:28 +0300

    add9t

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const changeColorElementRef = document.querySelector('.widget')
+changeColorElementRef.classList.add('js-widget');
+const changeColorBtnRef = document.querySelector('.change-color');
+const currentColorTextRef = document.querySelector('.color');
+const docStyleRef = document.querySelector('style');
+
+changeColorBtnRef.addEventListener('click', getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let colorChoise = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  
+  docStyleRef.textContent += `.js-widget{ background-color: ${colorChoise}}`;
+  currentColorTextRef.textContent = `${colorChoise}`;
 }
