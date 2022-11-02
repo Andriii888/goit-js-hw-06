dadc78d5bdcaf0f0afe3a802424be1925fe74350
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-11-02 22:12:11 +0300

    add6t

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,13 @@
 const inputValidationRef = document.querySelector('#validation-input');
-const needLengthInputValue = inputValidationRef.getAttribute('data-length');
+const inputDataref = document.querySelector('input[data-length="6"]');
+const needLengthInputValue = inputDataref.dataset.length;
 
 
 inputValidationRef.addEventListener('blur',onValidInput);
 
 function onValidInput (event){
     let writeLengthValue = event.currentTarget.value.length;
-    writeLengthValue === 6 ?
+    writeLengthValue == needLengthInputValue ?
                inputValidationRef.classList.replace('invalid','valid') ||
         inputValidationRef.classList.add('valid') :
         inputValidationRef.classList.add('invalid')
