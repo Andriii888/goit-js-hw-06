8c217bc5636cbc63b11290a631259e75bf8da42d
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-11-05 13:18:58 +0300

    add7

--- js/task-07.js
+++ js/task-07.js
@@ -1,22 +1,13 @@
 const inputRangeRef = document.querySelector('#font-size-control');
 const sizeTextRef = document.querySelector('#text');
-sizeTextRef.classList.add('inputTextSize');
-
-
 
 inputRangeRef.addEventListener('input', onTextSizeReplace);
 
  
 function onTextSizeReplace(event) {
-    let styleForText = document.createElement('style');
-    document.head.appendChild(styleForText);
     let inputSize = event.target.value;
 
-    styleForText.textContent = `.inputTextSize{font-size:${inputSize}px}}`;
 
-    // ТАК НЕ ПРАЦЮЄ  В МЕНЕ НЕМАЄ ТАКОГО ПАРАМЕТРА styleTextRef
-    // на відео там виділений sizeTextRef
-    // sizeTextRef.style.fontSize = `${inputSize}px}`;
-    // ТАК НЕ ПРАЦЮЄ 
+    sizeTextRef.style.fontSize = `${inputSize}px`
 }
 
